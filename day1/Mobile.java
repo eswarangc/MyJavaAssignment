b052fe6923c2e3876693abbb9e8047e773998273
eswarangc <111970407+eswarangc@users.noreply.github.com>
2023-08-13 08:48:28 +0530

    Add files via upload
    day 1

---
+++ day1/Mobile.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+	
+		int price =12000;
+		boolean  iphonemodel= true;
+		String name ="Iphone";
+		char Logo= 'A';
+		float weight = 99.6f;
+				System.out.println("Model of the mobile:"+name+"\n"+price+"\n"+iphonemodel+"\n"+Logo+"\n"+weight);
+			
+				
+		
+	}
+
+}
