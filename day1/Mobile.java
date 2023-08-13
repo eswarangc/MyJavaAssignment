5d4b62b16d7b04770a34bc43b8baa3032bc79e12
eswarangc <111970407+eswarangc@users.noreply.github.com>
2023-08-13 10:10:16 +0530

    Add files via upload

--- day1/Mobile.java
+++ day1/Mobile.java
@@ -1,18 +1,38 @@
 package week1.day1;
 
 public class Mobile {
-
-	public static void main(String[] args) {
+	
+		public static void main(String[] args) {
 	
 		int price =12000;
 		boolean  iphonemodel= true;
 		String name ="Iphone";
 		char Logo= 'A';
 		float weight = 99.6f;
-				System.out.println("Model of the mobile:"+name+"\n"+price+"\n"+iphonemodel+"\n"+Logo+"\n"+weight);
-			
-				
+				//System.out.println("Model of the mobile:"+name+"\n"+price+"\n"+iphonemodel+"\n"+Logo+"\n"+weight);
+			Mobile data =new Mobile();
+			data.sendSms();
+			data.makecall(price);
+						
 		
 	}
 
+		private int makecall(int price) {
+			// TODO Auto-generated method stub
+			int result= price;
+			System.out.println(result);
+			return(result);
+		}
+
+		private void sendSms() 
+		
+	{
+			// TODO Auto-generated method stub
+			
+			System.out.println("From Iphone");
+			
+		}
+
 }
+		
+
